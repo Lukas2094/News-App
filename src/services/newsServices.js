8e95e77c994e7047e008ad94396a56d78223ab83
lucassilvaativo <lucas.silva@ativo.com>
2024-02-02 11:49:17 -0300

    teste vercel

--- src/services/newsServices.js
+++ src/services/newsServices.js
@@ -5,7 +5,7 @@ const API_KEY = '49139bd361594812a1dc0210ca5147a4';
 const newsService = {
   getNews: async () => {
     try {
-      const url = `https://newsapi.org/v2/everything?q=Corinthians&language=pt&apiKey=${API_KEY}&pageSize=500`;
+      const url = `https://newsapi.org/v2/everything?q=Corinthians&language=pt&apiKey=${API_KEY}&pageSize=100`;
       const response = await axios.get(url);
       return response.data.articles;
     } catch (error) {
