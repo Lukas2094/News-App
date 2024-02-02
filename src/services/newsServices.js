f4226348127d55dcae0bc9b11e11836c600edf12
lucassilvaativo <lucas.silva@ativo.com>
2024-02-02 14:23:34 -0300

    add new responde api

--- src/services/newsServices.js
+++ src/services/newsServices.js
@@ -1,15 +1,23 @@
 import axios from 'axios';
 
 const API_KEY = '49139bd361594812a1dc0210ca5147a4';
+const BASE_URL = 'https://newsapi.org/v2';
 
 const newsService = {
   getNews: async () => {
     try {
-      const url = `https://newsapi.org/v2/everything?q=Corinthians&language=pt&apiKey=${API_KEY}&pageSize=100`;
-      const response = await axios.get(url);
+      const response = await axios.get(`${BASE_URL}/everything`, {
+        params: {
+          q: 'Corinthians',
+          language: 'pt',
+          apiKey: API_KEY,
+          pageSize: 100,
+        },
+      });
+
       return response.data.articles;
     } catch (error) {
-      console.error('Error fetching news:', error);
+      console.error('Error fetching news:', error.message);
       throw error;
     }
   },
